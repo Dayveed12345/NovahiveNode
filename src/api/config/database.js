32e09259d0de7be1b0838d4b6151023cf1e69fbb
dayveeed12345 <nwinyinyadavid123@gmail.com>
2024-06-02 21:52:10 +0100

    Added some files

--- src/api/config/database.js
+++ src/api/config/database.js
@@ -3,6 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME,process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      ca: fs.readFileSync('ssl.pem') // Path to your SSL certificate file
+    }
+  }
 });
 export default sequelize;
