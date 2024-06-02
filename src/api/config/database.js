b97007935463f537f4d68b9e12d7b28ecf59eb1a
dayveeed12345 <nwinyinyadavid123@gmail.com>
2024-06-02 20:20:47 +0100

    Added some files

--- src/api/config/database.js
+++ src/api/config/database.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME,process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'postgres'
+  dialect: 'mysql'
 });
 export default sequelize;
