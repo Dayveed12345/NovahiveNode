90eb679ae126c39c92157e11982297cf96860673
dayveeed12345 <nwinyinyadavid123@gmail.com>
2024-06-02 18:27:07 +0100

    Made changes to the dotenv file

--- src/api/config/config.js
+++ src/api/config/config.js
@@ -1,7 +1,7 @@
 const development = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
 };
@@ -9,7 +9,7 @@ const development = {
 const test = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
 };
@@ -17,7 +17,7 @@ const test = {
 const production = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
 };
