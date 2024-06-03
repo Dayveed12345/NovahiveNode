9fd25e72e8588258ed0f905e971ed19387a27643
dayveeed12345 <nwinyinyadavid123@gmail.com>
2024-06-03 12:01:04 +0100

    Removed the ssl configuration

--- src/api/config/database.js
+++ src/api/config/database.js
@@ -3,11 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME,process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      ca: fs.readFileSync('ssl.pem') // Path to your SSL certificate file
-    }
-  }
+  dialect: 'mysql'
 });
 export default sequelize;
